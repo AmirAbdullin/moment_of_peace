4423a6bd19d80d62acac470a9fd29da8a3d148bb
AmirAbdullin <83095319+AmirAbdullin@users.noreply.github.com>
2023-04-26 21:49:06 +0300

    Add files via upload

---
+++ precache-manifest.2631266ad5bc500476a183f1b2c95cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "224201bdcad44306c67e",
+    "url": "/momentofpeace/static/css/main.1725294c.chunk.css"
+  },
+  {
+    "revision": "224201bdcad44306c67e",
+    "url": "/momentofpeace/static/js/main.50da8b00.chunk.js"
+  },
+  {
+    "revision": "cfbafe2ee06f8c201ad5",
+    "url": "/momentofpeace/static/js/runtime~main.b1f96ab6.js"
+  },
+  {
+    "revision": "5004e11e068a8d230f63",
+    "url": "/momentofpeace/static/css/2.927f5086.chunk.css"
+  },
+  {
+    "revision": "5004e11e068a8d230f63",
+    "url": "/momentofpeace/static/js/2.dfbf29c9.chunk.js"
+  },
+  {
+    "revision": "74f12b158a98bd9602388df76fdc1f12",
+    "url": "/momentofpeace/index.html"
+  }
+];
